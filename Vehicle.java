336c7143b13bec758432cbc49cae9779883dac78
NatalieR2026 <natalie.radin2026@gbwl.org>
2025-03-04 13:55:57 +0000

    3/4

---
+++ Vehicle.java
@@ -0,0 +1,12 @@
+/* Vehicle is a SUPERCLASS (PARENT)
+Put the most GENERAL attributes +
+behaviors in this class definition
+Java classes can have MULTIPLE CHILDREN, but ONLY ONE PARENT
+ */
+public class Vehicle {
+    // 1. INSTANCE VARIABLES
+    private int numWheels;
+    private double avgSpeed;
+    private String color;
+
+}
