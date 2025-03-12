3a4e1e2a08d3884bdec14112e6554dc70c66790c
NatalieR2026 <natalie.radin2026@gbwl.org>
2025-03-12 13:42:50 +0000

    3/12

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 public class Main {
 
    public static void main(String []args) {
@@ -19,6 +20,28 @@ public class Main {
       v2.makeNoise();
       c2.makeNoise(); // OVERRIDE parent method
       
+      // POLYMORPHISM example:
+      // all classes in a hierarchy can be treated as
+      // instances of the TOP (most super) type
+      Vehicle v3 = new Vehicle();
+      Vehicle c3 = new Car(); // Car IS-A type of Vehicle
+      // Car bad = new Vehicle(); DOES NOT WORK.
+      Vehicle scoot = new Scooter();
+      Vehicle moto = new Motorcycle();
+      Vehicle convertible = new Convertible();
+
+      // POLYMORPHISM example:
+      // Can store any subclass in a superclass-type collection
+      ArrayList<Vehicle> vehicleList = new ArrayList<Vehicle>();
+      vehicleList.add(v1);
+      vehicleList.add(v2);
+      vehicleList.add(v3);
+      vehicleList.add(c1); // object constructed as a Car
+      vehicleList.add(c2); // but Cars are still Vehicles!
+      vehicleList.add(c3);
+      vehicleList.add(moto);
+      vehicleList.add(scoot);
+      System.out.println(vehicleList);
 
 
    }
